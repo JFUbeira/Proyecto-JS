5faed4d9369b21d716a176afa45bda199f9c5021
JuanFUbeira <franciscoubeira@gmail.com>
2023-08-28 10:26:46 -0300

    intento n1 de fix github pages ruta cart.html toastify

--- js/detail.js
+++ js/detail.js
@@ -46,15 +46,35 @@ const stock = await getStock()
 const shoppingCart = []
 
 function addToCart(product) {
-    let currentCart = JSON.parse(localStorage.getItem('shoppingCart')) || []
-    currentCart.push(product)
-    localStorage.setItem('shoppingCart', JSON.stringify(currentCart))
-    console.log(currentCart)
+    let currentCart = JSON.parse(localStorage.getItem('shoppingCart')) || [];
+    currentCart.push(product);
+    localStorage.setItem('shoppingCart', JSON.stringify(currentCart));
+    console.log(currentCart);
+    
+    // Obtener la URL base de GitHub Pages
+    const baseUrl = window.location.href.substring(0, window.location.href.lastIndexOf('/') + 1);
+    const cartPageUrl = baseUrl + 'cart.html'; // Agregar la ruta relativa a la página del carrito
+    
     Toastify({
-    text: 'Se agregó el producto al carrito. Haz click para ver el carrito ->',
-    duration: 3000,
-    destination: '../cart.html'  
-    }).showToast()
+        text: 'Se agregó el producto al carrito. Haz click para ver el carrito ->',
+        duration: 3000,
+        destination: cartPageUrl
+    }).showToast();
 }
+
+
+
+
+// function addToCart(product) {
+//     let currentCart = JSON.parse(localStorage.getItem('shoppingCart')) || []
+//     currentCart.push(product)
+//     localStorage.setItem('shoppingCart', JSON.stringify(currentCart))
+//     console.log(currentCart)
+//     Toastify({
+//     text: 'Se agregó el producto al carrito. Haz click para ver el carrito ->',
+//     duration: 3000,
+//     destination: '../cart.html'  
+//     }).showToast()
+// }
 
 
